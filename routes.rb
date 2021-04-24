84f602edc743ea3d513d756b92917331bffe7213
po-me117 <78285159+po-me117@users.noreply.github.com>
2021-04-25 08:24:37 +0900

    Delete routes.rb

--- routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :contacts
-  get "/" => "home#top"
-  get "/about" => "home#about"
-  get "/commitment" => "home#commitment"
-  get "/contact" => "home#contact"
-  get "/menu" => "home#menu"
-end
